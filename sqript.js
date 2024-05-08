b9cd7b1c1b5dcbed74d41d3b44d4d652e3837b22
akr771 <akr772129462@gmail.com>
2024-05-08 15:38:22 +0200

    first

---
+++ sqript.js
@@ -0,0 +1,11 @@
+ let age=16;
+ if(age>=18)
+   {  console.log('you are old enough to vote')}
+else 
+ {console.log('no')};
+
+let neme='akam';
+if(neme==='akram')
+  console.log('hallo akram')
+else 
+console.log('You are not akram.')
